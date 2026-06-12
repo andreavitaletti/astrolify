e3fe03edb2c748a1c81ee0c08c27d5f337fb20dd
andreavitaletti <vitaletti@dis.uniroma1.it>
2026-06-12 14:57:39 +0200

    change the base

--- astro.config.ts
+++ astro.config.ts
@@ -5,8 +5,9 @@ import { siteConfig } from "./src/config/site.config";
 export default defineConfig({
   // 1. Tell Astro the production URL (essential for the sitemap and SEO)
   //site: siteConfig.seo.siteUrl,
-  site: "https://andreavitaletti.github.io/",
-  base: "/seedpeers",
+  //site: "https://andreavitaletti.github.io/",
+  //base: "/seedpeers",
+  site: "https://seedpeers.net/",
   // 2. Point the build output to your 'docs' folder
   //outDir: "./docs",
   // 3. Keep output as 'static' (this is Astro's default, but good to keep explicit)
