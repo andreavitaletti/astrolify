0093d0ac841c11f82f655834fa95948371f2d002
andreavitaletti <vitaletti@dis.uniroma1.it>
2026-06-12 13:13:06 +0200

    first version deployable on github pages

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import { siteConfig } from './src/config/site.config'
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import { siteConfig } from "./src/config/site.config";
 
 export default defineConfig({
   site: siteConfig.seo.siteUrl,
   integrations: [sitemap()],
-})
+  outDir: "./docs", // <-- Add this line
+});
---
+++ docs/_astro/utils.DXguSzbD.js
@@ -0,0 +1,1 @@
+const d=e=>{document.readyState==="loading"?document.addEventListener("DOMContentLoaded",e):e()};export{d as D};
